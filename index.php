5a4638676c2348c26b2a7030f17637bd8298f1b1
Jenya Melnik <Jenyamelnik1986@i.ua>
2021-05-31 23:27:56 +0300

    feat Создан общий файл входа

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Заголовок страницы</title>
+    <meta name="description" content="Описание страницы">
+    <meta name="keywords" content="Ключевые слова через запятую">
+    <link href="css/normalize.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<div>
+    <a href="/homework1/homework1.php">homework №1</a>
+    <a href="/homework2.php">homework №2</a>
+    <a href="/homework3/homework3.php">homework №3</a>
+    <a href="/homework10/homework10.php">homework №10</a>
+    <a href="/homework11/homework11.php">homework №11</a>
+    <a href="/homework13.php">homework №13</a>
+    <a href="/homework14/">homework №14</a>
+</div>
+</body>
+</html>
+
