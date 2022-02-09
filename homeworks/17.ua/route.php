15f524204309f877ad1e9d50b2a213d44938a157
Jenya Melnik <Jenyamelnik1986@i.ua>
2022-02-09 15:33:34 +0200

    feat: HW 38 was corrected

--- homeworks/17.ua/route.php
+++ homeworks/17.ua/route.php
@@ -6,17 +6,3 @@
 
 $modulePath = './' . Core::$CONTROLLER . '/' . $module . '/' . $page . '.php';
 $skinPath = './skins/' . Core::$SKIN . '/' . $module . '/' . $page . '.tpl';
-
-/*
-if (!file_exists($modulePath)) {
-    $module = 'errors';
-    $page = '404';
-    $modulePath = './modules/' . $module . '/' . $page . '.php';
-}
-
-if (!file_exists($skinPath)) {
-    $module = 'errors';
-    $page = '404';
-    $skinPath = './skins/' . Core::$SKIN . '/' . $module . '/' . $page . '.tpl';
-}
-*/
