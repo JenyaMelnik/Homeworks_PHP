c6337ba5966dea3bd7d1c16171ce193ccf87d732
Jenya Melnik <Jenyamelnik1986@i.ua>
2022-02-09 17:52:30 +0200

    feat: HW 38 was corrected

---
+++ homeworks/17.ua/modules/comments/ajaxAddComments.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['login'], $_POST['comment'], $_POST['email'])) {
+    $comment = [
+        'login' => htmlspecialchars($_POST['login']),
+        'email' => htmlspecialchars($_POST['email']),
+        'comment' => nl2br(htmlspecialchars($_POST['comment'])),
+        'date' => date("Y-m-d H:i:s")
+    ];
+
+    $addCommentSqlQuery = " INSERT INTO `comments` SET
+            `login`   = '" . escapeString($_POST['login']) . "',
+            `email`   = '" . escapeString($_POST['email']) . "',
+            `comment` = '" . escapeString($_POST['comment']) . "',
+            `date`    = '" . $comment['date'] . "'";
+
+    if (query($addCommentSqlQuery)) {
+        $comment['id'] = mysqli_insert_id(DBConnectAndClose::connect());
+        echo json_encode($comment);
+        exit();
+    }
+}
--- homeworks/17.ua/modules/comments/main.php
+++ homeworks/17.ua/modules/comments/main.php
@@ -1,27 +1,4 @@
 <?php
-// ======================================= Добавление комментария ==================================================
-if (isset($_POST['login'], $_POST['comment'], $_POST['email'])) {
-    $comment = [
-        'login' => htmlspecialchars($_POST['login']),
-        'email' => htmlspecialchars($_POST['email']),
-        'comment' => nl2br(htmlspecialchars($_POST['comment'])),
-        'date' => date("Y-m-d H:i:s")
-    ];
-
-    $addCommentSqlQuery = " INSERT INTO `comments` SET
-            `login`   = '" . escapeString($_POST['login']) . "',
-            `email`   = '" . escapeString($_POST['email']) . "',
-            `comment` = '" . escapeString($_POST['comment']) . "',
-            `date`    = '" . $comment['date'] . "'";
-
-    if (query($addCommentSqlQuery)) {
-        $comment['id'] = mysqli_insert_id(DBConnectAndClose::connect());
-        echo json_encode($comment);
-        exit();
-    }
-}
-
-// =========================================== Все комментарии =====================================================
 $allCommentsQuery = query( "SELECT * FROM `comments` ORDER BY `id` DESC ");
 
 // =================================================================================================================
